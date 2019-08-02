d2fccad872dcc2510f29d885f921584733bf4f5f
Ezeki Zibzibadze <ez@jok.io>
2019-08-02 13:38:52 +0400

    refactor: remove e2e testing

--- apps/nx-angular-capacitor-seed-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('nx-angular-capacitor-seed', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to nx-angular-capacitor-seed!');
-  });
-});
