e980d49fa2743e76f59299cc0c18a9bd12b6a188
Ezeki Zibzibadze <ez@jok.io>
2019-08-02 18:12:59 +0400

    fix: implement component name during generation

--- apps/nx-angular-capacitor-seed/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'nx-angular-capacitor-seed',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/nx-angular-capacitor-seed',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- tools/schematics/new-component/index.ts
+++ tools/schematics/new-component/index.ts
@@ -2,6 +2,7 @@ import { chain, externalSchematic, Rule, Tree, SchematicsException } from "@angu
 import * as path from 'path'
 import { getWorkspace } from '../utility/workspace'
 import { strings } from '@angular-devkit/core';
+import { parseName } from '../utility/parseName';
 
 export default function (schema: any): Rule {
 
@@ -14,16 +15,15 @@ export default function (schema: any): Rule {
     }
 
     const projectType = project.extensions['projectType']
-    console.log('projectType', projectType, typeof projectType)
 
     if (!['application', 'library'].includes(projectType.toString())) {
       throw new SchematicsException(`Invalid project type in angular.json, allowed: [application, library]`);
     }
 
-    let pathUrl
+    let basePathUrl
 
     if (projectType === 'application') {
-      pathUrl = path.join(
+      basePathUrl = path.join(
         'apps',
         schema.project,
         'src',
@@ -32,19 +32,21 @@ export default function (schema: any): Rule {
     }
 
     if (projectType === 'library') {
-      pathUrl = path.join(
+      basePathUrl = path.join(
         'libs',
         schema.project,
         'src',
-        'lib',
+        // 'lib',
       )
     }
 
+    const { name, path: pathUrl } = parseName(basePathUrl, schema.name)
+
     return chain([
-      createStorybookFile(pathUrl, schema.name),
+      createStorybookFile(pathUrl, name),
       externalSchematic("@schematics/angular", "component", {
         project: schema.project,
-        name: schema.name,
+        name,
         path: pathUrl,
         skipImport: true,
         skipTests: true,
@@ -57,15 +59,17 @@ export default function (schema: any): Rule {
 function createStorybookFile(pathUrl, name) {
   return (tree: Tree) => {
 
-    const classifiedName = strings.classify(`${name}Component`);
+    const pureName = strings.dasherize(name)
+    const classifiedName = strings.classify(`${pureName}Component`);
 
-    const fullPath = path.join(pathUrl, name, name + '.stories.ts')
+    const fullPath = path.join(pathUrl, name, pureName + '.stories.ts')
+
     const content = `import { storiesOf } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 import { ${classifiedName} } from './${name}.component';
 
 storiesOf('${classifiedName}', module)
-  .add('to Storybook', () => ({
+  .add('normal', () => ({
     component: ${classifiedName},
     props: {
       click: action('clicked'),
