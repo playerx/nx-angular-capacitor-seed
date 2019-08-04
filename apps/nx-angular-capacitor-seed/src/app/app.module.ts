d7a1677d94637ad2ee75aab5aebbe0da1ec74767
Ezeki Zibzibadze <ez@jok.io>
2019-08-04 14:13:19 +0400

    fixes

--- apps/nx-angular-capacitor-seed/src/app/app.module.ts
+++ apps/nx-angular-capacitor-seed/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { IonicModule } from '@ionic/angular'
 
 import { AppComponent } from './app.component'
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(),
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
--- tools/schematics/new-component/index.ts
+++ tools/schematics/new-component/index.ts
@@ -1,70 +1,78 @@
-import { chain, externalSchematic, Rule, Tree, SchematicsException } from "@angular-devkit/schematics"
+import { strings } from '@angular-devkit/core'
+import {
+	chain,
+	externalSchematic,
+	Rule,
+	SchematicsException,
+	Tree,
+} from '@angular-devkit/schematics'
 import * as path from 'path'
+import { parseName } from '../utility/parseName'
 import { getWorkspace } from '../utility/workspace'
-import { strings } from '@angular-devkit/core';
-import { parseName } from '../utility/parseName';
 
 export default function (schema: any): Rule {
 
-  return async (host: Tree) => {
+	return async (host: Tree) => {
 
-    const workspace = await getWorkspace(host);
-    const project = workspace.projects.get(schema.project as string);
-    if (!project) {
-      throw new SchematicsException(`Invalid project name (${schema.project})`);
-    }
+		const workspace = await getWorkspace(host)
+		const project = workspace.projects.get(schema.project as string)
+		if (!project) {
+			throw new SchematicsException(`Invalid project name (${schema.project})`)
+		}
 
-    const projectType = project.extensions['projectType']
+		const projectType = project.extensions['projectType']
 
-    if (!['application', 'library'].includes(projectType.toString())) {
-      throw new SchematicsException(`Invalid project type in angular.json, allowed: [application, library]`);
-    }
+		if (!['application', 'library'].includes(projectType.toString())) {
+			throw new SchematicsException(
+				`Invalid project type in angular.json, allowed: [application, library]`,
+			)
+		}
 
-    let basePathUrl
+		let basePathUrl
 
-    if (projectType === 'application') {
-      basePathUrl = path.join(
-        'apps',
-        schema.project,
-        'src',
-        'app',
-      )
-    }
+		if (projectType === 'application') {
+			basePathUrl = path.join(
+				'apps',
+				schema.project,
+				'src',
+				'app',
+			)
+		}
 
-    if (projectType === 'library') {
-      basePathUrl = path.join(
-        'libs',
-        schema.project,
-        'src',
-        // 'lib',
-      )
-    }
+		if (projectType === 'library') {
+			basePathUrl = path.join(
+				'libs',
+				schema.project,
+				'src',
+				// 'lib',
+			)
+		}
 
-    const { name, path: pathUrl } = parseName(basePathUrl, schema.name)
+		const { name, path: pathUrl } = parseName(basePathUrl, schema.name)
 
-    return chain([
-      createStorybookFile(pathUrl, name),
-      externalSchematic("@schematics/angular", "component", {
-        project: schema.project,
-        name,
-        path: pathUrl,
-        skipImport: true,
-        skipTests: true,
-        styleext: 'scss',
-      })
-    ])
-  }
+		return chain([
+			createStorybookFile(pathUrl, name),
+			externalSchematic('@schematics/angular', 'component', {
+				project: schema.project,
+				name,
+				path: pathUrl,
+				skipImport: true,
+				skipTests: true,
+				styleext: 'scss',
+			}),
+		])
+	}
 }
 
 function createStorybookFile(pathUrl, name) {
-  return (tree: Tree) => {
+	return (tree: Tree) => {
 
-    const pureName = strings.dasherize(name)
-    const classifiedName = strings.classify(`${pureName}Component`);
+		const pureName = strings.dasherize(name)
+		const classifiedName = strings.classify(`${pureName}Component`)
 
-    const fullPath = path.join(pathUrl, name, pureName + '.stories.ts')
+		const fullPath = path.join(pathUrl, name, pureName + '.stories.ts')
 
-    const content = `import { storiesOf } from '@storybook/angular';
+		const content = `import { storiesOf } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 import { ${classifiedName} } from './${name}.component';
 
@@ -77,8 +85,8 @@ storiesOf('${classifiedName}', module)
   }));
   `
 
-    tree.create(fullPath, content)
+		tree.create(fullPath, content)
 
-    return tree
-  }
+		return tree
+	}
 }
